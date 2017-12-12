8d4733e0b607ca6e67fdc4e7efc59afa46b0a0c5
Dan Nolllette <nollette.dan@gmail.com>
2017-12-12 14:11:44 -0800

    modify quotes/get to allow search by parameter

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,26 @@
 class QuotesController < ApplicationController
 
   def index
-    @quotes = Quote.all
+    # @quotes = Quote.all
+    # puts "params: "
+    if !params[:first_season]
+      first_season = 0
+    else
+      first_season = params[:first_season].to_i
+    end
+    if !params[:last_season]
+      last_season = 100
+    else
+      last_season = params[:last_season].to_i
+    end
+    character = params[:character]
+    character ||= ""
+    character = "%" + character + "%"
+    content = params[:content]
+    content ||= ''
+    content = "%" + content + "%"
+    @quotes = Quote.where("season_number >= :first_season AND season_number <= :last_season AND character LIKE :character AND content LIKE :content",
+    {first_season: first_season, last_season: last_season, character: character, content: content})
     json_response(@quotes, 200)
   end
 
@@ -29,6 +48,6 @@ class QuotesController < ApplicationController
 private
 
   def quote_params
-    params.permit(:content, :character, :episode_of_season, :season_number)
+    params.permit(:content, :character, :episode_of_season, :season_number, :first_season, :last_season)
   end
 end
